c7bc83e1e459f60cddeabb362c4dca2d5a9d0c74
hejiawen <hjw0415@outlook.com>
2020-07-22 16:55:47 +0800

    基础部分步进电机例程修改完成

--- basis_part/stepper—PWM控制匀速旋转/User/stepper/bsp_stepper_init.c
+++ basis_part/stepper—PWM控制匀速旋转/User/stepper/bsp_stepper_init.c
@@ -21,23 +21,6 @@
 void TIM_SetTIMxCompare(TIM_TypeDef *TIMx,uint32_t channel,uint32_t compare);
 void TIM_SetPWM_period(TIM_TypeDef* TIMx,uint32_t TIM_period);
 
-TIM_HandleTypeDef TIM_TimeBaseStructure;
-//__IO uint16_t OC_Pulse_num = 500;     //比较输出的计数值
-__IO uint16_t OC_Pulse_num = 200;     //比较输出的计数值
-
-
- /**
-  * @brief  中断优先级配置
-  * @param  无
-  * @retval 无
-  */
-static void TIMx_NVIC_Configuration(void)
-{
-  /* 外设中断配置 */
-  HAL_NVIC_SetPriority(MOTOR_PUL_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(MOTOR_PUL_IRQn);
-}
-
 /**
   * @brief  配置TIM复用输出PWM时用到的I/O
   * @param  无
@@ -50,22 +33,21 @@ static void Stepper_GPIO_Config(void)
 	MOTOR_DIR_GPIO_CLK_ENABLE();
 	MOTOR_PUL_GPIO_CLK_ENABLE();
 	MOTOR_EN_GPIO_CLK_ENABLE();
-
+	
 	/*选择要控制的GPIO引脚*/															   
 	GPIO_InitStruct.Pin = MOTOR_DIR_PIN;	
 	/*设置引脚的输出类型为推挽输出*/
-	GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;  
+	GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP; 
 	GPIO_InitStruct.Pull =GPIO_PULLUP;
 	/*设置引脚速率为高速 */   
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 	/*Motor 方向引脚 初始化*/
 	HAL_GPIO_Init(MOTOR_DIR_GPIO_PORT, &GPIO_InitStruct);	
-
+	
 	/*Motor 使能引脚 初始化*/
 	GPIO_InitStruct.Pin = MOTOR_EN_PIN;	
 	HAL_GPIO_Init(MOTOR_EN_GPIO_PORT, &GPIO_InitStruct);	
-
-
+	
 	/* 定时器通道1功能引脚IO初始化 */
 	/*设置输出类型*/
 	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -76,9 +58,9 @@ static void Stepper_GPIO_Config(void)
 	/*设置复用*/
 	GPIO_InitStruct.Pull =GPIO_PULLUP;
 	/*选择要控制的GPIO引脚*/	
-	GPIO_InitStruct.Pin = MOTOR_PUL_PIN;
+	GPIO_InitStruct.Pin = GENERAL_TIM_CH1_PIN;
 	/*Motor 脉冲引脚 初始化*/
-	HAL_GPIO_Init(MOTOR_PUL_PORT, &GPIO_InitStruct);			
+	HAL_GPIO_Init(GENERAL_TIM_CH1_GPIO_PORT, &GPIO_InitStruct);			
 }
 
 
@@ -94,83 +76,46 @@ static void Stepper_GPIO_Config(void)
  * TIM_RepetitionCounter TIMx,x[1,8]才有(高级定时器)
  *-----------------------------------------------------------------------------
  */
-void TIM_PWMOUTPUT_Config(void)
+
+TIM_OC_InitTypeDef  TIM_OCInitStructure;  
+TIM_HandleTypeDef  TIM_TimeBaseStructure;	
+static void TIM_PWMOUTPUT_Config(void)
 {
-	TIM_OC_InitTypeDef  TIM_OCInitStructure;  	
+	
+	int tim_per=1000;//定时器周期
+
 	/*使能定时器*/
 	MOTOR_PUL_CLK_ENABLE();
 
-	TIM_TimeBaseStructure.Instance = MOTOR_PUL_TIM;    
+	TIM_TimeBaseStructure.Instance = MOTOR_PUL_TIM;
 	/* 累计 TIM_Period个后产生一个更新或者中断*/		
 	//当定时器从0计数到10000，即为10000次，为一个定时周期
-	TIM_TimeBaseStructure.Init.Period = TIM_PERIOD; 
-	//定时器时钟源TIMxCLK = 2 * PCLK1  
-	//				PCLK1 = HCLK / 4 
-	//				=> TIMxCLK=HCLK/2=SystemCoreClock/2=240MHz
+	TIM_TimeBaseStructure.Init.Period = tim_per;
+    //定时器时钟源TIMxCLK = 2 * PCLK1  
+    //				PCLK1 = HCLK / 4 
+    //				=> TIMxCLK=HCLK/2=SystemCoreClock/2=240MHz
 	// 设定定时器频率为=TIMxCLK/(TIM_Prescaler+1)=1MHz
-	TIM_TimeBaseStructure.Init.Prescaler = 240-1;                
+	TIM_TimeBaseStructure.Init.Prescaler = 240-1;	
 
 	/*计数方式*/
-	TIM_TimeBaseStructure.Init.CounterMode = TIM_COUNTERMODE_UP;            
-	/*采样时钟分频*/	
-	TIM_TimeBaseStructure.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;   
-	TIM_TimeBaseStructure.Init.RepetitionCounter = 0 ;  		
+	TIM_TimeBaseStructure.Init.CounterMode = TIM_COUNTERMODE_UP;
+	/*采样时钟分频*/
+	TIM_TimeBaseStructure.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;
 	/*初始化定时器*/
-	HAL_TIM_OC_Init(&TIM_TimeBaseStructure);
+	HAL_TIM_Base_Init(&TIM_TimeBaseStructure);
 
-	/*PWM模式配置--这里配置为输出比较模式*/
-	TIM_OCInitStructure.OCMode = TIM_OCMODE_TOGGLE; 
-	/*比较输出的计数值*/
-	TIM_OCInitStructure.Pulse = OC_Pulse_num;                    
+	/*PWM模式配置*/
+	TIM_OCInitStructure.OCMode = TIM_OCMODE_PWM1;//配置为PWM模式1 
+	TIM_OCInitStructure.Pulse = tim_per/2;//默认占空比为50%
+	TIM_OCInitStructure.OCFastMode = TIM_OCFAST_DISABLE;
 	/*当定时器计数值小于CCR1_Val时为高电平*/
-	TIM_OCInitStructure.OCPolarity = TIM_OCPOLARITY_HIGH;          
-	/*设置互补通道输出的极性*/
-	TIM_OCInitStructure.OCNPolarity = TIM_OCNPOLARITY_LOW; 
-	/*快速模式设置*/
-	TIM_OCInitStructure.OCFastMode = TIM_OCFAST_DISABLE;   
-	/*空闲电平*/
-	TIM_OCInitStructure.OCIdleState = TIM_OCIDLESTATE_RESET;  
-	/*互补通道设置*/
-	TIM_OCInitStructure.OCNIdleState = TIM_OCNIDLESTATE_RESET; 
-	HAL_TIM_OC_ConfigChannel(&TIM_TimeBaseStructure, &TIM_OCInitStructure, MOTOR_PUL_CHANNEL_x);
+	TIM_OCInitStructure.OCPolarity = TIM_OCPOLARITY_HIGH;	
 
-	/* 确定定时器 */
-	HAL_TIM_Base_Start(&TIM_TimeBaseStructure);
-	/* 启动比较输出并使能中断 */
-	HAL_TIM_OC_Start_IT(&TIM_TimeBaseStructure,MOTOR_PUL_CHANNEL_x);
-	/*使能比较通道*/
-	TIM_CCxChannelCmd(MOTOR_PUL_TIM,MOTOR_PUL_CHANNEL_x,TIM_CCx_ENABLE);
-
-}
-
-/**
-  * @brief  定时器中断函数
-	*	@note 		无
-  * @retval 无
-  */
-void MOTOR_PUL_IRQHandler(void)
-{
-  HAL_TIM_IRQHandler(&TIM_TimeBaseStructure);
-}
-
-/**
-  * @brief  定时器比较中断
-  * @param  htim：定时器句柄指针
-	*	@note 		无
-  * @retval 无
-  */
-void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  uint32_t count;
-  __IO uint32_t temp_val;
-	/*获取当前计数*/
-  count=__HAL_TIM_GET_COUNTER(&TIM_TimeBaseStructure);
-	/*计算比较数值*/
-  temp_val = TIM_PERIOD & (count+OC_Pulse_num); 
-		
-	/*设置比较数值*/
-  __HAL_TIM_SET_COMPARE(&TIM_TimeBaseStructure,MOTOR_PUL_CHANNEL_x,temp_val);
-	
+	/*配置PWM通道*/
+	HAL_TIM_PWM_ConfigChannel(&TIM_TimeBaseStructure, &TIM_OCInitStructure, MOTOR_PUL_CHANNEL_x);
+	/*开始输出PWM*/
+	HAL_TIM_PWM_Start(&TIM_TimeBaseStructure,MOTOR_PUL_CHANNEL_x);
+			
 }
 
 /**
@@ -180,11 +125,11 @@ void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim)
 	*	@note 	无
   * @retval 无
   */
-void TIM8_SetPWM_pulse(int channel,int compare)
+void TIM2_SetPWM_pulse(int channel,int compare)
 {
 	switch(channel)
 	{
-		case 1:	  __HAL_TIM_SET_COMPARE(&TIM_TimeBaseStructure,TIM_CHANNEL_1,compare);break;
+		case 1:   __HAL_TIM_SET_COMPARE(&TIM_TimeBaseStructure,TIM_CHANNEL_1,compare);break;
 		case 2:	  __HAL_TIM_SET_COMPARE(&TIM_TimeBaseStructure,TIM_CHANNEL_2,compare);break;
 		case 3:	  __HAL_TIM_SET_COMPARE(&TIM_TimeBaseStructure,TIM_CHANNEL_3,compare);break;
 		case 4:	  __HAL_TIM_SET_COMPARE(&TIM_TimeBaseStructure,TIM_CHANNEL_4,compare);break;
@@ -199,12 +144,27 @@ void TIM8_SetPWM_pulse(int channel,int compare)
   */
 void stepper_Init()
 {
-	/*电机IO配置*/
 	Stepper_GPIO_Config();
-	/*定时器PWM输出配置*/
+
 	TIM_PWMOUTPUT_Config();
-	/*中断配置*/
-	TIMx_NVIC_Configuration();
+						
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
