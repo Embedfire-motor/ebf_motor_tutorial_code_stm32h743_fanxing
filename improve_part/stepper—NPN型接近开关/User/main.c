7627ba57a8f727de08fb3bb6e749168e8169799a
hejiawen <hjw0415@outlook.com>
2020-07-28 15:06:23 +0800

    增加npn接近开关例程

---
+++ improve_part/stepper—NPN型接近开关/User/main.c
@@ -0,0 +1,138 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2020-xx-xx
+  * @brief   步进电机-GPIO中断模拟脉冲控制
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32H743 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************
+  */  
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32h7xx.h"
+#include "main.h"
+#include "./usart/bsp_debug_usart.h"
+#include "./delay/core_delay.h"
+#include "./switch/bsp_proximity_switch.h"
+#include "./led/bsp_led.h"
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void) 
+{
+	/* 初始化系统时钟为480MHz */
+	SystemClock_Config();
+	/*初始化USART 配置模式为 115200 8-N-1，中断接收*/
+	DEBUG_USART_Config();
+	printf("欢迎使用野火 电机开发板 接近开关 例程\r\n");
+  /* 初始化时间戳 */
+  HAL_InitTick(5);
+	/*led初始化*/
+	LED_GPIO_Config();
+  /* 接近开关初始化 */
+  ProximitySwitch_Config();
+	
+	while(1)
+	{
+    HAL_Delay(10);
+	}
+} 	
+
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 480000000 (CPU Clock)
+	*            HCLK(Hz)             = 240000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  120MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  120MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  120MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  120MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 192
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** 启用电源配置更新
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** 配置主内稳压器输出电压
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  /** 初始化CPU、AHB和APB总线时钟
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+		while(1);
+  }
+  /** 初始化CPU、AHB和APB总线时钟
+  */
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4)!= HAL_OK)
+  {
+    while(1) { ; }
+  }
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_FDCAN;
+  PeriphClkInitStruct.FdcanClockSelection = RCC_FDCANCLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+		while(1);
+  }
+}
+/****************************END OF FILE***************************/
