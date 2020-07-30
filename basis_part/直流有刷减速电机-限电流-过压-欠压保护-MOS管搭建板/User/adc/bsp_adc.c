d40bee1edcbe98edf59bf0cc421d90fe657e6b54
jc <798025823@qq.com>
2020-07-30 17:01:23 +0800

    添加过压-欠压保护例程,修改模拟看门狗

---
+++ basis_part/直流有刷减速电机-限电流-过压-欠压保护-MOS管搭建板/User/adc/bsp_adc.c
@@ -0,0 +1,309 @@
+#include "./adc/bsp_adc.h"
+#include ".\motor_control\bsp_motor_control.h"
+#include "./led/bsp_led.h" 
+#include "./usart/bsp_debug_usart.h"
+
+__IO uint16_t ADC_ConvertedValue;
+DMA_HandleTypeDef DMA_Init_Handle;
+ADC_HandleTypeDef ADC_Handle;
+
+static uint32_t adc_buff[ADC_NUM_MAX];    // 电压采集缓冲区
+static uint32_t vbus_adc_mean = 0;        // 电源电压 ACD 采样结果平均值
+static uint32_t adc_mean_sum = 0;        // 平均值累加
+static uint32_t adc_mean_count = 0;      // 累加计数
+
+/**
+  * @brief  ADC 通道引脚初始化
+  * @param  无
+  * @retval 无
+  */
+static void ADC_GPIO_Config(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    // 使能 GPIO 时钟
+    CURR_ADC_GPIO_CLK_ENABLE();
+    VBUS_GPIO_CLK_ENABLE();
+    // 配置 IO
+    GPIO_InitStructure.Pin = CURR_ADC_GPIO_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_ANALOG;	    
+    GPIO_InitStructure.Pull = GPIO_NOPULL ; //不上拉不下拉
+    HAL_GPIO_Init(CURR_ADC_GPIO_PORT, &GPIO_InitStructure);	
+
+    GPIO_InitStructure.Pin = VBUS_GPIO_PIN;
+    HAL_GPIO_Init(VBUS_GPIO_PORT, &GPIO_InitStructure);	
+}
+
+void adc_dma_init(void)
+{
+    // ------------------DMA Init 结构体参数 初始化--------------------------
+    // ADC1使用DMA2，数据流0，通道0，这个是手册固定死的
+    // 开启DMA时钟
+    CURR_ADC_DMA_CLK_ENABLE();
+    // 数据传输通道
+    DMA_Init_Handle.Instance = CURR_ADC_DMA_STREAM;
+    // 数据传输方向为外设到存储器	
+    DMA_Init_Handle.Init.Direction = DMA_PERIPH_TO_MEMORY;
+    // 外设寄存器只有一个，地址不用递增
+    DMA_Init_Handle.Init.PeriphInc = DMA_PINC_DISABLE;
+    // 存储器地址固定
+    DMA_Init_Handle.Init.MemInc = DMA_MINC_ENABLE;
+    // 外设数据大小为字，即四个字节,H7上的DR寄存器为32位,处理不好数据会异常,用字对应寄存器大小即可
+    DMA_Init_Handle.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+    // 存储器数据大小也为字，跟外设数据大小相同
+    DMA_Init_Handle.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;	
+    // 循环传输模式
+    DMA_Init_Handle.Init.Mode = DMA_CIRCULAR;
+    // DMA 传输通道优先级为高，当使用一个DMA通道时，优先级设置不影响
+    DMA_Init_Handle.Init.Priority = DMA_PRIORITY_HIGH;
+    // 禁止DMA FIFO	，使用直连模式
+    DMA_Init_Handle.Init.FIFOMode = DMA_FIFOMODE_DISABLE;  
+    // FIFO 大小，FIFO模式禁止时，这个不用配置
+    DMA_Init_Handle.Init.FIFOThreshold = DMA_FIFO_THRESHOLD_HALFFULL;
+    DMA_Init_Handle.Init.MemBurst = DMA_MBURST_SINGLE;
+    DMA_Init_Handle.Init.PeriphBurst = DMA_PBURST_SINGLE;  
+    // 选择 DMA 通道，通道存在于流中
+    DMA_Init_Handle.Init.Request = DMA_REQUEST_ADC1; 
+    //初始化DMA流，流相当于一个大的管道，管道里面有很多通道
+    HAL_DMA_Init(&DMA_Init_Handle); 
+
+    __HAL_LINKDMA(&ADC_Handle,DMA_Handle,DMA_Init_Handle);
+}
+
+/**
+  * @brief  ADC 和 DMA 初始化
+  * @param  无
+  * @retval 无
+  */
+static void ADC_Mode_Config(void)
+{
+    // 开启ADC时钟
+    CURR_ADC_CLK_ENABLE();
+    // -------------------ADC Init 结构体 参数 初始化------------------------
+    // ADC1
+    ADC_Handle.Instance = CURR_ADC;
+    // 时钟为fpclk 4分频	
+    ADC_Handle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
+    // ADC 分辨率
+    ADC_Handle.Init.Resolution = ADC_RESOLUTION_16B;
+    // 禁止扫描模式，多通道采集才需要	
+    ADC_Handle.Init.ScanConvMode = ADC_SCAN_ENABLE; 
+    // 连续转换	
+    ADC_Handle.Init.ContinuousConvMode = ENABLE;
+    // 非连续转换	
+    ADC_Handle.Init.DiscontinuousConvMode = DISABLE;
+    // 非连续转换个数
+    ADC_Handle.Init.NbrOfDiscConversion   = 0;
+    //禁止外部边沿触发    
+    ADC_Handle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    //使用软件触发
+    ADC_Handle.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    //数据左移,不左移
+    ADC_Handle.Init.LeftBitShift = ADC_LEFTBITSHIFT_NONE;
+    //转换通道 2个
+    ADC_Handle.Init.NbrOfConversion = 2;
+		//转换数据管理:DMA单次传输
+		ADC_Handle.Init.ConversionDataManagement = ADC_CONVERSIONDATA_DMA_ONESHOT;
+		//过采样
+		ADC_Handle.Init.OversamplingMode = DISABLE;
+    //数据溢出,对原数据数据处理方式,覆写
+    ADC_Handle.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+    //转换完成标志
+    ADC_Handle.Init.EOCSelection = ADC_EOC_SINGLE_CONV;    
+    //初始化ADC
+    HAL_ADC_Init(&ADC_Handle);
+
+		/* 校准 ADC，采用偏移校准 */
+		if (HAL_ADCEx_Calibration_Start(&ADC_Handle, ADC_CALIB_OFFSET, ADC_SINGLE_ENDED) != HAL_OK)
+		{
+			printf("%s,%d\r\n",__FILE__, __LINE__);
+			while(1);
+		}
+		
+    /** 配置AD模拟看门狗
+    */
+    ADC_AnalogWDGConfTypeDef AnalogWDGConfig = {0};
+    /* 使用AD模拟看门狗一 */
+		AnalogWDGConfig.WatchdogNumber = ADC_ANALOGWATCHDOG_1;
+		/* 只监测一个规则通道 */
+    AnalogWDGConfig.WatchdogMode = ADC_ANALOGWATCHDOG_SINGLE_REG;
+		/* 阈值上限 */
+    AnalogWDGConfig.HighThreshold = VBUS_HEX_MAX;
+		/* 阈值下限 */
+    AnalogWDGConfig.LowThreshold = VBUS_HEX_MIN;
+		/* 检测通道 */
+    AnalogWDGConfig.Channel = VBUS_ADC_CHANNEL;
+		/* 使能模拟看门狗中断,溢出会产生中断回调 */
+    AnalogWDGConfig.ITMode = ENABLE;
+    if (HAL_ADC_AnalogWDGConfig(&ADC_Handle, &AnalogWDGConfig) != HAL_OK)
+    {
+      while(1);
+    }
+		
+    //---------------------------------------------------------------------------
+    ADC_ChannelConfTypeDef ADC_Config;
+    
+    ADC_Config.Channel      = CURR_ADC_CHANNEL;
+    // 配置 ADC 通道转换顺序为1，第一个转换，采样时间为3个时钟周期
+    ADC_Config.Rank         = ADC_REGULAR_RANK_1;
+    // 采样时间间隔	
+    ADC_Config.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+		// 采样输入方式,单端输入
+		ADC_Config.SingleDiff = ADC_SINGLE_ENDED;
+		// 采样数据自定义偏移,不偏移
+    ADC_Config.Offset       = 0;
+		ADC_Config.OffsetNumber = ADC_OFFSET_NONE;
+		ADC_Config.OffsetRightShift = DISABLE;
+		ADC_Config.OffsetSignedSaturation = DISABLE;
+
+    HAL_ADC_ConfigChannel(&ADC_Handle, &ADC_Config);
+    
+    /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+    */
+    ADC_Config.Channel = VBUS_ADC_CHANNEL;
+    ADC_Config.Rank = ADC_REGULAR_RANK_2;
+		ADC_Config.SingleDiff = ADC_SINGLE_ENDED;
+    // 采样时间间隔	
+    ADC_Config.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+    ADC_Config.Offset       = 0;
+    if (HAL_ADC_ConfigChannel(&ADC_Handle, &ADC_Config) != HAL_OK)
+    {
+      while(1);
+    }
+		
+    // 外设中断优先级配置和使能中断配置
+    HAL_NVIC_SetPriority(ADC_DMA_IRQ, 1, 1);
+    HAL_NVIC_EnableIRQ(ADC_DMA_IRQ);
+		
+		HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(ADC_IRQn);
+		
+    HAL_ADC_Start_DMA(&ADC_Handle, (uint32_t*)&adc_buff, ADC_NUM_MAX);
+}
+
+/**
+  * @brief  电流采集初始化
+  * @param  无
+  * @retval 无
+  */
+void ADC_Init(void)
+{
+	ADC_GPIO_Config();
+  adc_dma_init();
+	ADC_Mode_Config();
+}
+
+/**
+  * @brief  常规转换在非阻塞模式下完成回调
+  * @param  hadc: ADC  句柄.
+  * @retval 无
+  */
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+  int32_t adc_mean = 0;
+
+//  HAL_ADC_Stop_DMA(hadc);       // 停止 ADC 采样，处理完一次数据在继续采样,使用DMA单次模式不停止
+
+  /* 计算电流通道采样的平均值 */
+  for(uint32_t count = 0; count < ADC_NUM_MAX; count+=2)
+  {
+    adc_mean += adc_buff[count];
+  }
+  
+  adc_mean_sum += adc_mean / (ADC_NUM_MAX / 2);    // 累加电压
+  adc_mean_count++;
+  
+#if 1
+  
+  adc_mean = 0;
+  
+  /* 计算电压通道采样的平均值 */
+  for(uint32_t count = 1; count < ADC_NUM_MAX; count+=2)
+  {
+    adc_mean += adc_buff[count];
+  }
+  
+  vbus_adc_mean = adc_mean / (ADC_NUM_MAX / 2);    // 保存平均值
+
+#else
+  vbus_adc_mean = adc_buff[1];
+#endif
+  HAL_ADC_Start_DMA(&ADC_Handle, (uint32_t*)&adc_buff, ADC_NUM_MAX);    // 重新开始 ADC 采样
+
+}
+
+static uint16_t flag_num = 0;
+/**
+  * @brief  在非阻塞模式模拟看门狗回调
+  * @param  hadc: ADC  句柄.
+  * @retval 无
+  */
+void HAL_ADC_LevelOutOfWindowCallback(ADC_HandleTypeDef* hadc)
+{
+	float temp_adc;
+	
+  flag_num++;     // 电源电压超过阈值电压
+	
+  temp_adc = get_vbus_val();
+	
+  if (temp_adc > VBUS_MIN && temp_adc < VBUS_MAX)
+    flag_num = 0;
+  
+  if (flag_num > ADC_NUM_MAX)      // 电源电压超过阈值电压10次
+  {
+    set_motor_disable();
+    flag_num = 0;
+    LED1_ON;
+    printf("电源电压超过限制！请检查原因，复位开发板在试！\r\n");
+    while(1);
+  }
+}
+
+/**
+  * @brief  获取电流值
+  * @param  无
+  * @retval 转换得到的电流值
+  */
+int32_t get_curr_val(void)
+{
+  static uint8_t flag = 0;
+  static uint32_t adc_offset = 0;    // 偏置电压
+  int16_t curr_adc_mean = 0;         // 电流 ACD 采样结果平均值
+  
+  curr_adc_mean = adc_mean_sum / adc_mean_count;    // 保存平均值
+  
+
+    adc_mean_count = 0;
+    adc_mean_sum = 0;
+    
+    if (flag < 17)
+    {
+      adc_offset = curr_adc_mean;    // 多次记录偏置电压，待系统稳定偏置电压才为有效值
+      flag += 1;
+    }
+    if(curr_adc_mean>=adc_offset)
+	{
+		curr_adc_mean -= adc_offset;                     // 减去偏置电压
+	}else
+	{
+		curr_adc_mean=0;
+	}
+
+
+  float vdc = GET_ADC_VDC_VAL(curr_adc_mean);      // 获取电压值
+  
+  return GET_ADC_CURR_VAL(vdc);
+}
+
+/**
+  * @brief  获取电源电压值
+  * @param  无
+  * @retval 转换得到的电流值
+  */
+float get_vbus_val(void)
+{
+  float vdc = GET_ADC_VDC_VAL(vbus_adc_mean);      // 获取电压值
+  
+  return GET_VBUS_VAL(vdc);
+}
+
+/*********************************** END OF FILE *********************************************/
---
+++ basis_part/直流有刷减速电机-限电流-过压-欠压保护-MOS管搭建板/User/main.c
@@ -0,0 +1,231 @@
+/**
+  ******************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2020-xx-xx
+  * @brief   直流有刷减速电机-电压电流读取-MOS管搭建板
+  ******************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32H743 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************
+  */  
+#include <stdio.h>
+#include <stdlib.h>
+#include "stm32h7xx.h"
+#include "main.h"
+#include "./led/bsp_led.h"
+#include "./usart/bsp_debug_usart.h"
+#include "./key/bsp_key.h"
+#include "./tim/bsp_motor_tim.h"
+#include ".\motor_control\bsp_motor_control.h"
+#include "./adc/bsp_adc.h"
+
+/* 宏定义 */
+#define CURR_MAX    200    // 最大电流（单位mA）
+
+/**
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  */
+int main(void) 
+{
+  __IO uint16_t ChannelPulse = PWM_MAX_PERIOD_COUNT*0.5;
+  uint8_t curr_max_count = 0;
+  uint8_t flag = 0;
+  uint8_t dir = 0;
+
+  HAL_Init();
+  
+	/* 初始化系统时钟为168MHz */
+	SystemClock_Config();
+  
+	/* 初始化按键GPIO */
+	Key_GPIO_Config();
+  
+  /* 初始化 LED */
+  LED_GPIO_Config();
+
+  /* 电机初始化 */
+  motor_init();
+  
+  /* 串口初始化 */
+  DEBUG_USART_Config();
+//	for(volatile int i=0x2fffff;i>0;i--){}
+
+  /* ADC 始化 */
+  ADC_Init();
+  
+  set_motor_speed(ChannelPulse);
+  set_motor_disable();    // 禁用电机
+  printf("野火直流有刷电机-限流-过压-欠压保护实验\r\n");
+	
+	while(1)
+	{
+    /* 扫描KEY1 */
+    if( Key_Scan(KEY1_GPIO_PORT, KEY1_PIN) == KEY_ON)
+    {
+      /* 使能电机 */
+      set_motor_enable(); 
+    }
+    
+    /* 扫描KEY2 */
+    if( Key_Scan(KEY2_GPIO_PORT, KEY2_PIN) == KEY_ON)
+    {
+      /* 禁用电机 */
+      set_motor_disable();
+    }
+    
+    /* 扫描KEY3 */
+    if( Key_Scan(KEY3_GPIO_PORT, KEY3_PIN) == KEY_ON)
+    {
+      /* 增大占空比 */
+      ChannelPulse += PWM_MAX_PERIOD_COUNT/10;
+      
+      if(ChannelPulse > PWM_MAX_PERIOD_COUNT)
+        ChannelPulse = PWM_MAX_PERIOD_COUNT;
+      
+      set_motor_speed(ChannelPulse);
+    }
+    
+    /* 扫描KEY4 */
+    if( Key_Scan(KEY4_GPIO_PORT, KEY4_PIN) == KEY_ON)
+    {
+      if(ChannelPulse < PWM_MAX_PERIOD_COUNT/10)
+        ChannelPulse = 0;
+      else
+        ChannelPulse -= PWM_MAX_PERIOD_COUNT/10;
+      
+      set_motor_speed(ChannelPulse);
+    }
+    
+    /* 扫描KEY5 */
+    if( Key_Scan(KEY5_GPIO_PORT, KEY5_PIN) == KEY_ON)
+    {
+      /* 转换方向 */
+      set_motor_direction( (++dir % 2) ? MOTOR_FWD : MOTOR_REV);
+    }
+    
+    if (HAL_GetTick()%50 == 0 && flag == 0)    // 每50毫秒读取一次电流、电压
+    {
+      flag = 1;
+      int32_t current = get_curr_val();
+
+    #if 0//defined(PID_ASSISTANT_EN)
+      set_computer_value(SEED_FACT_CMD, CURVES_CH1, &current, 1);
+    #else
+      printf("电源电压：%.2fV，电流：%dmA\r\n", get_vbus_val(), current); 
+    #endif
+      
+      if (current > CURR_MAX)    // 判断是不是超过限定的值
+      {
+        if (curr_max_count++ > 5)    // 连续5次超过
+        {
+          LED2_ON;
+          set_motor_disable();
+          curr_max_count = 0;
+          printf("电流超过限制！请检查原因，复位开发板在试！\r\n");
+          while(1);
+        }
+      }
+      
+    }
+    else if (HAL_GetTick()%50 != 0 && flag == 1)
+    {
+      flag = 0;
+    }
+	}
+}
+
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 480000000 (CPU Clock)
+	*            HCLK(Hz)             = 240000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  120MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  120MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  120MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  120MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 192
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** 启用电源配置更新
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** 配置主内稳压器输出电压
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  /** 初始化CPU、AHB和APB总线时钟
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+		while(1);
+  }
+  /** 初始化CPU、AHB和APB总线时钟
+  */
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4)!= HAL_OK)
+  {
+    while(1) { ; }
+  }
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_FDCAN;
+  PeriphClkInitStruct.FdcanClockSelection = RCC_FDCANCLKSOURCE_PLL;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+		while(1);
+  }
+}
+/****************************END OF FILE***************************/
